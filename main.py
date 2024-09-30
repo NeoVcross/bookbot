307a968b7944fc363b0267de927abc27f7696a24
Neocross <zhou-nicolas@hotmail.com>
2024-09-30 23:42:53 +0200

    count_characters

--- main.py
+++ main.py
@@ -5,12 +5,21 @@ def count_words(text):
         count += 1
     return count
 
+def count_characters(text):
+    dico = {}
+    for character in text:
+        character = character.lower()
+        if character not in dico:
+            dico[character] = 0
+        dico[character] += 1
+    return dico
 
 
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
         print(count_words(file_contents))
+        print(count_characters(file_contents))
 
 if __name__ == '__main__':
     main()

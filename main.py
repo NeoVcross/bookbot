aff31060523f0386d58dc0e507cd1d3109559cbf
Neocross <zhou-nicolas@hotmail.com>
2024-09-30 22:35:45 +0200

    count_words

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-
+def count_words(text):
+    count = 0
+    text = text.split()
+    for _ in text:
+        count += 1
+    return count
 
 
 
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        print(file_contents)
+        print(count_words(file_contents))
 
 if __name__ == '__main__':
     main()
